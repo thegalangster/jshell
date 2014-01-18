731fc0189401de7f84963e1a589dad6509b5e540
Sean Spearman <seanmspearman@gmail.com>
2014-01-17 19:53:08 -0800

    Add struct Pipeline and possible new and delete functions for it.

--- Command.h
+++ Command.h
@@ -4,13 +4,35 @@
 typedef struct Command Command;
 
 struct Command {
-  char *input_file;
-  char *output_file;
+  char *input_file; // Will these file vars be set to NULL if they're not used?
+  char *output_file; // I don't think they will implicitly, at least not reliably.
   char *append_file;
   int background;
   char **newargv;
 
   Command *next;
 };
+
+// Can we use this to contain the linked list of commands?
+typdef struct {
+  Command* head;
+  int length;
+} Pipeline;
+
+// length needs to be initialized to 0 when a new pipeline is created,
+// so you could use something like this
+Pipeline* newPipeline() {
+  Pipeline* newpl = (Pipeline *)malloc(sizeof(Pipeline));
+  newpl->length = 0;
+  newpl->head = (Command *)malloc(sizeof(Command));
+  return newpl;
+}
+// and this to free
+void deletePipeline(Pipeline *pl) {
+  free(pl->head);
+  free(pl);
+}
+// NOTE: NEITHER OF THESE ARE TESTED
+
 
 #endif
